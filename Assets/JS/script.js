a42efd8ce0bd0e244056ce9f838f0a94c28a1ce6
Niloufar Sabbaghian <niloufarsabbaghian@Niloufars-iMac.local>
2022-09-18 11:55:04 -0700

    saved hour input event created

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,18 +2,29 @@
 var today = moment();
 $('#currentDay').text(today.format("dddd, MMMM Do YYYY"));
 
-var hourLine =  document.querySelector('.time-block');
-var currentHour = today.format('H');
-console.log(currentHour);
+// save hours events to local storage
+$('.saveInput').on('click', function () {
+    console.log('after click');
+    // var time = $(this).siblings('.hour').val();
+    var time = $(this).parent().attr('id');
+    console.log(time);
+    var value = $(this).prev('.description').val();
+    console.log(value);
+    
+    localStorage.setItem(time, value);
+})
 
-var hr = [];
+var currentHour = today.format('HH');
+
+var hr;
 checkHour();
 
+//Update hour color differentiation
 function checkHour(){
     $('.time-block').each(function(){
         hr = ($(this).attr('id').slice(-2));
-        console.log(hr);
-        console.log(currentHour);
+        // console.log("hr = " + hr);
+        // console.log("currenthr = " + currentHour);
         if(hr < currentHour){
             $(this).addClass('past');
         } else if ( hr === currentHour){
@@ -23,4 +34,6 @@ function checkHour(){
         }
     })
 }
+
+
 
